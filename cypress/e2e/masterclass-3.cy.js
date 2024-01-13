91e9087a98d85ecd76e72a1b1c977972979b4fd5
Nicolas <nicolasmoen@live.com>
2024-01-13 18:55:39 -0300

    modificando archivo masterclass

--- cypress/e2e/masterclass-3.cy.js
+++ cypress/e2e/masterclass-3.cy.js
@@ -3,6 +3,8 @@ describe('Masterclass 3 Pushing it',()=>{
   it('Deberia visitar la pagina',()=>{
     var numeroRandom= Math.floor(Math.random()*1000)
     var user= "pushing" +numeroRandom
+    var tarea1 = "Tarea 1"
+    var tarea2 = "Tarea 2"
     cy.visit('https://pushing-front.vercel.app/')
     cy.get('#user').type(user );
     cy.get('#pass').type("123456!");
@@ -15,6 +17,11 @@ describe('Masterclass 3 Pushing it',()=>{
    cy.get(`[id^='user_${user}_']`).should("exist");
    cy.get(`[id*='${user}']`).should("exist");
    cy.xpath('//a[starts-with(@id,"todolist")]').click()
-   
+   cy.xpath("//input[@cy-get='task']").type(tarea1)
+   cy.xpath("//input[@cy-get='task']/following-sibling::button").click()
+   cy.xpath("//input[@cy-get='task']").type(tarea2)
+   cy.xpath("//input[@cy-get='task']/following-sibling::button").click()
+   cy.xpath(`//p[text()='${tarea1}']/following-sibling::button`).click()
+   cy.xpath(`//p[text()='${tarea2}']`).click()
   })  
 })
