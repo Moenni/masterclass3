ed44e9f1fd2e7837e245dbabbd7a9f3444e381b3
Nicolas <nicolasmoen@live.com>
2024-01-13 19:40:31 -0300

    terminando masterclass3

--- cypress/e2e/masterclass-3.cy.js
+++ cypress/e2e/masterclass-3.cy.js
@@ -1,6 +1,6 @@
 ///<reference types="cypress"/>
 describe('Masterclass 3 Pushing it',()=>{
-  it('Deberia visitar la pagina',()=>{
+  it.skip('Deberia visitar la pagina',()=>{
     var numeroRandom= Math.floor(Math.random()*1000)
     var user= "pushing" +numeroRandom
     var tarea1 = "Tarea 1"
@@ -23,5 +23,35 @@ describe('Masterclass 3 Pushing it',()=>{
    cy.xpath("//input[@cy-get='task']/following-sibling::button").click()
    cy.xpath(`//p[text()='${tarea1}']/following-sibling::button`).click()
    cy.xpath(`//p[text()='${tarea2}']`).click()
+   cy.contains(tarea2).siblings('button').click()
   })  
+  it("Deberia probar la funcionalidad de todo list",()=>{
+    var tarea1= "Tarea 1"
+    var tarea2= "Tarea 2"
+    var tarea3= "Tarea 3"
+    var tarea4= "Tarea 4"
+    var tarea5= "Tarea 5"
+    var tarea6= "Tarea 6"
+    cy.visit('https://pushing-front.vercel.app/')
+    cy.get("#registertoggle").dblclick()
+    cy.xpath("//input[@name='user']").type("pushingit")
+    cy.xpath("//input[@name='pass']").type("123456!")
+    cy.contains("Log in").click()
+    cy.get(`[id*='pushing']`).should("exist")
+    cy.get("#todolistlink").click()
+    cy.xpath("//input[@cy-get='task']").type(`${tarea1}{enter}`)
+    cy.xpath("//input[@cy-get='task']").type(`${tarea2}{enter}`)
+    cy.xpath("//input[@cy-get='task']").type(`${tarea3}{enter}`)
+    cy.xpath("//input[@cy-get='task']").type(`${tarea4}{enter}`)
+    cy.xpath("//input[@cy-get='task']").type(`${tarea5}{enter}`)
+    cy.xpath("//input[@cy-get='task']").type(`${tarea6}{enter}`)
+    cy.contains(tarea1).click()
+    cy.xpath(`//p[text()="${tarea2}"]`).click()
+    cy.xpath(`//p[starts-with(text(),"${tarea3}")]`).click()
+    cy.get("#completed").click()
+    cy.xpath("//p").should("have.length","3")
+    cy.get("#active").click()
+    cy.xpath("//ul[@role='list']/li/div/p").should("have.length","3")
+
+  })
 })
